a8da4135b5e1dcb7533144c8a99e5684e9246b1b
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2023-01-05 15:16:44 +0100

    add-list-and-list-items

---
+++ src/DummyData.js
@@ -0,0 +1,41 @@
+export const people = [
+  {
+    name: 'John Doe',
+    age: 54,
+    hairColor: 'brown',
+    hobbies: ['swimming', 'bicyclying', 'video games']
+  },
+  {
+    name: 'Micheal Chindwin',
+    age: 33,
+    hairColor: 'black',
+    hobbies: ['golf', 'mathematics',]
+  },
+  {
+    name: 'Jane Garcia',
+    age: 27,
+    hairColor: 'blonde',
+    hobbies: ['biology', 'medicine', 'gymnastics']
+  },
+]
+
+export const products =[
+  {
+    name: 'Flat-Screen TV',
+    price: '$300',
+    description: 'Huge LCD Screen, a great deal',
+    rating: 4.5
+  },
+  {
+    name: 'Basketball',
+    price: '$10',
+    description: 'Just like the pro use',
+    rating: 3.8
+  },
+  {
+    name: 'Running Shoes',
+    price: '$10',
+    description: 'State-of-the-art technology for optimum running',
+    rating: 4.2
+  },
+]
---
+++ src/components/List/products/LargeProductItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LargeProductListItem = ({ product: { name, price, description, rating } }) => {
+  return (
+    <>
+      <h3>{name}</h3>
+      <p>Price: ${price} years</p>
+      <h3>Description</h3>
+      <p>{description}</p>
+      <p>Rating: {rating}</p>
+      <hr />
+    </>
+  );
+};
+
+export default LargeProductListItem;
